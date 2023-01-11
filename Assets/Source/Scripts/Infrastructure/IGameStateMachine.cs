145ab6d31f585e0ea3c58145453a8ffc6aa83757
PabloEsk <pavelae27@mail.ru>
2023-01-11 23:24:08 +0300

    add buttons , add new shaderVariants

---
+++ Assets/Source/Scripts/Infrastructure/IGameStateMachine.cs
@@ -0,0 +1,10 @@
+﻿using Source.Infrastructure.States;
+
+namespace Source.Infrastructure
+{
+    public interface IGameStateMachine : IService
+    {
+        void Enter<TState>() where TState : class, IState;
+        void Enter<TState, TPayload>(TPayload payload) where TState : class, IPayloadState<TPayload>;
+    }
+}
