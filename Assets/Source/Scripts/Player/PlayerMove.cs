b59e8b174dfd298f70b6aa3f378ef257f4d75215
PabloEsk <pavelae27@mail.ru>
2023-01-17 16:32:06 +0300

    fix playerRotate

--- Assets/Source/Scripts/Player/PlayerMove.cs
+++ Assets/Source/Scripts/Player/PlayerMove.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using Dreamteck.Splines;
 using UnityEngine;
 
@@ -7,16 +6,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _speedRotate;
 
     private ConstructSplineComputer _constructSplineComputer;
-    private SplineFollower _splineFollower;
     private TriggerGroup _triggerGroup;
-    private TurningPoints _turningPoints;
     private bool _move;
-    private int _number;
-    private float _currentSpeedRotate;
-    private float _elapsedTime;
+    private Player _player;
 
     public bool CanMove { get; set; }
 
@@ -25,9 +19,9 @@ public class PlayerMove : MonoBehaviour
 
     private void Awake()
     {
+        _player = GetComponent<Player>();
         SplineComputer spline = FindObjectOfType<SplineComputer>();
         _constructSplineComputer = GetComponent<ConstructSplineComputer>();
-        _splineFollower = GetComponent<SplineFollower>();
         _constructSplineComputer.Construct(spline);
         CreateSplineTrigger();
     }
@@ -37,12 +31,12 @@ public class PlayerMove : MonoBehaviour
         RemoveListenerSplineTrigger();
         Disabled?.Invoke();
     }
-
-    private void Start() =>
-        _turningPoints = GameObject.FindGameObjectWithTag("TurningPoints").GetComponent<TurningPoints>();
-
-    public void PlayMove() =>
+    
+    public void PlayMove()
+    {
         _constructSplineComputer.SetSpeed(_speed);
+        _player.PlayerRotate.RotateReturn();
+    }
 
     private void CreateSplineTrigger()
     {
@@ -58,56 +52,9 @@ public class PlayerMove : MonoBehaviour
             splineTrigger.RemoveListener(StopMove);
     }
 
-
-    private void StartRotate()
-    {
-        var value = _number;
-        var newVector = SetNewVector(value);
-
-        StartCoroutine(RotateSplineFollower(newVector));
-
-        _number++;
-    }
-
-
-    private IEnumerator RotateSplineFollower(Vector3 vector)
-    {
-        _currentSpeedRotate = 0;
-
-        while (_currentSpeedRotate <= _speedRotate)
-        {
-            yield return new WaitForSeconds(0.01f);
-            _currentSpeedRotate += 0.01f;
-            _splineFollower.motion.rotationOffset =
-                Vector3.Lerp(_splineFollower.motion.rotationOffset, vector, _currentSpeedRotate);
-            Debug.Log($"{_splineFollower.motion.rotationOffset}-{vector}");
-        }
-
-        Stop(RotateSplineFollower(vector));
-    }
-
-    private Vector3 SetNewVector(int value)
-    {
-        var turningPoint = _turningPoints.GetPoint(value);
-
-        if (turningPoint != null)
-        {
-            Vector3 directionToFace = turningPoint.transform.position - transform.position;
-            float angleBetween = Vector3.SignedAngle(directionToFace, Vector3.back, Vector3.up);
-            var newVector = new Vector3(0, -angleBetween, 0);
-            return newVector;
-        }
-
-        return _splineFollower.motion.rotationOffset;
-    }
-
-
-    private void Stop(IEnumerator enumerator) =>
-        StopCoroutine(enumerator);
-
     private void StopMove(SplineUser arg0)
     {
-        StartRotate();
+        _player.PlayerRotate.StartRotate();
         Stopped?.Invoke();
         CanMove = false;
         _constructSplineComputer.SetSpeed(0);
