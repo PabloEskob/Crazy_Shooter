c274649219920f06728be817bbba6db772e8565c
DpyrCoToHbl <D-Eagle@mail.ru>
2023-01-18 18:49:17 +0700

    add GA SDK and scripts for analytic

---
+++ Assets/Source/Scripts/Analytics/IAnalyticManager.cs
@@ -0,0 +1,25 @@
+﻿using Source.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Source.Scripts.Analytics
+{
+    public interface IAnalyticManager : IService
+    {
+        void AddAnalytic(IAnalytic analytic);
+        void SendEventOnGameInitialize(int sessionCount);
+        void SendEventOnLevelStart(int levelNumber);
+        void SendEventOnLevelComplete(int levelNumber);
+        void SendEventOnFail(int levelNumber);
+        void SendEventOnLevelRestart(int levelNumber);
+        void SendEventOnSoftSpent(string purchaseType, string storeName, int purchaseAmount, int purchasesCount);
+        void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame);
+        void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame, int currentSoft);
+        void SendEventContentIsOver(int sessionCount, int daysInGame);
+        void SendEvent(string eventName, Dictionary<string, object> dataObjects);
+        void SendEvent(string eventName);
+    }
+}
