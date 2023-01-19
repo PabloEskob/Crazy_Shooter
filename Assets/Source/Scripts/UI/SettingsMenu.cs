d72c9fa464fa7e9d8b6952307be10bd53d19203c
DpyrCoToHbl <D-Eagle@mail.ru>
2023-01-19 13:02:36 +0700

    add sound settings, save and load

--- Assets/Source/Scripts/UI/SettingsMenu.cs
+++ Assets/Source/Scripts/UI/SettingsMenu.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Source.Scripts.UI;
 using Source.Infrastructure;
+using Source.Scripts.Data;
 using Source.Scripts.Infrastructure.Services.PersistentProgress;
 using System;
 using Unity.VisualScripting;
@@ -18,6 +19,10 @@ namespace Source.Scripts.Ui
         [SerializeField] private Button _turkishLanguageButton;
         [SerializeField] private Button _exitButton;
 
+        [Header("Slider values settings")]
+        [SerializeField] private float _minValue = 0.1f;
+        [SerializeField] private float _maxValue = 2.0f;
+
         IStorage _storage;
 
         private const int DefaultSliderValue = 1;
@@ -25,15 +30,24 @@ namespace Source.Scripts.Ui
         private void Awake()
         {
             _storage = AllServices.Container.Single<IStorage>();
-            _cameraSensitivitySlider.minValue = 0.1f;
-            _cameraSensitivitySlider.maxValue = 2.0f;
+            _cameraSensitivitySlider.minValue = _minValue;
+            _cameraSensitivitySlider.maxValue = _maxValue;
+            OnLoad();
+            Hide();
+        }
 
+        private void OnLoad()
+        {
             if (_storage.HasKeyFloat(SettingsNames.SensitivityKey))
                 SetSliderValue(_storage.GetFloat(SettingsNames.SensitivityKey));
             else
                 SetSliderValue(DefaultSliderValue);
 
-            Hide();
+            if (_storage.HasKeyInt(SettingsNames.SoundSettingsKey))
+                _soundToggle.isOn = DataExtensions.IntToBool(_storage.GetInt(SettingsNames.SoundSettingsKey));
+
+            if (_storage.HasKeyInt(SettingsNames.MusicSettingsKey))
+                _musicToggle.isOn = DataExtensions.IntToBool(_storage.GetInt(SettingsNames.MusicSettingsKey));
         }
 
         private void OnEnable()
@@ -42,7 +56,8 @@ namespace Source.Scripts.Ui
             _englishLaguageButton.onClick.AddListener(SwitchLanguage);
             _russianLanguageButton.onClick.AddListener(SwitchLanguage);
             _turkishLanguageButton.onClick.AddListener(SwitchLanguage);
-            _cameraSensitivitySlider.onValueChanged.AddListener(SaveSliderSettings);
+            _soundToggle.onValueChanged.AddListener(OnToogleSwitched);
+            _cameraSensitivitySlider.onValueChanged.AddListener(OnSensitivitySliderValueChanged);
         }
 
         private void OnDisable()
@@ -52,11 +67,14 @@ namespace Source.Scripts.Ui
             _englishLaguageButton.onClick.RemoveListener(SwitchLanguage);
             _russianLanguageButton.onClick.RemoveListener(SwitchLanguage);
             _turkishLanguageButton.onClick.RemoveListener(SwitchLanguage);
-            _cameraSensitivitySlider.onValueChanged.RemoveListener(SaveSliderSettings);
-            _storage.Save();
+            _soundToggle.onValueChanged.RemoveListener(OnToogleSwitched);
+            _cameraSensitivitySlider.onValueChanged.RemoveListener(OnSensitivitySliderValueChanged);
         }
 
-        private void SaveSliderSettings(float value) =>
+        private void OnToogleSwitched(bool value) => 
+            _storage.SetInt(SettingsNames.SoundSettingsKey, DataExtensions.BoolToInt(value));
+
+        private void OnSensitivitySliderValueChanged(float value) =>
             _storage.SetFloat(SettingsNames.SensitivityKey, value);
 
         private void SwitchLanguage()
@@ -64,7 +82,7 @@ namespace Source.Scripts.Ui
             throw new NotImplementedException();
         }
 
-        private void SetSliderValue(float value) => 
+        private void SetSliderValue(float value) =>
             _cameraSensitivitySlider.value = value;
 
         private void Hide() =>
